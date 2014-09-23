0ab52c57ed846f0c73fd8d24a807b9caa9405d7e
Haixing Hu <starfish.hu@gmail.com>
2014-09-23 23:14:08 +0800

    add the DisplayName model

---
+++ src/main/java/com/github/haixing_hu/ilibrary/model/DisplayName.java
@@ -0,0 +1,89 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2014  Haixing Hu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.github.haixing_hu.ilibrary.utils.jaxb;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+/**
+ * The model of a mapping entry from the name of a locale to the corresponding
+ * display name.
+ *
+ * @author Haixing Hu
+ */
+@XmlType(name="display-name")
+public final class DisplayName {
+
+  @XmlAttribute(name="locale")
+  private String locale;
+
+  @XmlValue
+  private String name;
+
+
+  public DisplayName() {
+    locale = null;
+    name = null;
+  }
+
+  public DisplayName(String locale, String name) {
+    this.locale = locale;
+    this.name = name;
+  }
+
+  /**
+   * Gets the locale.
+   *
+   * @return the locale.
+   */
+  public String getLocale() {
+    return locale;
+  }
+
+  /**
+   * Sets the locale.
+   *
+   * @param locale
+   *          the new locale to set.
+   */
+  public void setLocale(String locale) {
+    this.locale = locale;
+  }
+
+  /**
+   * Gets the name.
+   *
+   * @return the name.
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Sets the name.
+   *
+   * @param name
+   *          the new name to set.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
---
+++ src/main/java/com/github/haixing_hu/ilibrary/model/DisplayNames.java
@@ -0,0 +1,91 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2013  Haixing Hu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.github.haixing_hu.ilibrary.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+ * The model of list of display names.
+ *
+ * @author Haixing Hu
+ */
+@XmlRootElement(name="display-names")
+@XmlAccessorType(XmlAccessType.FIELD)
+public final class DisplayNames implements Iterable<DisplayName> {
+
+  @XmlElement(name="display-name")
+  private final List<DisplayName> names;
+
+
+  public DisplayNames() {
+    names = new ArrayList<DisplayName>();
+  }
+
+  public DisplayNames(int initialCapacity) {
+    names = new ArrayList<DisplayName>(initialCapacity);
+  }
+
+  public int size() {
+    return names.size();
+  }
+
+  public boolean isEmpty() {
+    return names.isEmpty();
+  }
+
+  @Override
+  public Iterator<DisplayName> iterator() {
+    return names.iterator();
+  }
+
+  public void add(DisplayName name) {
+    names.add(name);
+  }
+
+  public String get(Locale locale) {
+
+  }
+
+  @Override
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return EqualsBuilder.reflectionEquals(this, obj);
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
+}
